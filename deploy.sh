d0932a47a223b716bf2e841644da7b2595bd36bc
Juan Pablo Barreto <juampi92@gmail.com>
2015-09-11 00:18:06 -0300

    Small deploy.sh error fix

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,8 @@ git config user.email "juampi92@gmail.com"
 
 # Add changes (based on the replacement done earlier)
 git add .
-git commit -m "Deploy to GitHub Pages" # It'd be good to add a deploy version to the commit message
+git commit -m "Deploy to GitHub Pages" || true # Or true, so if there's nothing to commit, don't trow an error
 
 # Force push gh-pages branch (current one) with the changes made from the build
 # We redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1 || true
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
