5ecaf97847fbe0c0f2fc5c3cea9a8ec24b3dbf44
Juan Pablo Barreto <juampi92@gmail.com>
2015-09-14 11:00:41 -0300

    removed unused module

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-yuidoc');
-  grunt.loadNpmTasks('grunt-contrib-handlebars');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-browserify');
